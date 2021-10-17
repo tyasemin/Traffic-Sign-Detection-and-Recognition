329cfb60b4b3fdcbbc52a12b00569c6aa7d916d6
Yasemin Tangül <70871014+tyasemin@users.noreply.github.com>
2021-10-18 01:25:55 +0300

    upload

---
+++ traffic_sign_detection/json_modified.py
@@ -0,0 +1,25 @@
+import os
+import tqdm
+import json
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+JSON_TXT_DIR=os.path.join(__location__,"allLabel.txt")
+
+#read file (path and label)
+file=open(JSON_TXT_DIR,"r")
+allLabel_raw=file.readlines() #iterable
+file.close()
+
+#implement strip function for each line
+allLabel=[]
+file=open(os.path.join(__location__,"allLabel_modified.txt"),"w")
+for line in allLabel_raw:
+    line=line.strip()
+    
+    if line.find(" ") !=-1: #if json has bbox
+        file.write(line+"\n")
+
+
+file.close()
